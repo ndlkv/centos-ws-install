f3512061fb7524b0c1ead7d95213451921adfb2e
Nedyalkov <ndlkv@users.noreply.github.com>
2015-07-28 18:28:54 +0200

    Update ws-script.sh

--- ws-script.sh
+++ ws-script.sh
@@ -45,7 +45,7 @@ server {
     index index.php index.html index.htm;
 
     location / {
-        try_files $uri $uri/ =404;
+        try_files \$uri \$uri/ =404;
     }
     error_page 404 /404.html;
     error_page 500 502 503 504 /50x.html;
@@ -54,10 +54,10 @@ server {
     }
 
     location ~ \.php$ {
-        try_files $uri =404;
+        try_files \$uri =404;
         fastcgi_pass unix:/var/run/php-fpm/php-fpm.sock;
         fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
     }
 }
